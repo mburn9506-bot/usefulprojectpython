83596663431267837c58497e00b93d4a97da37c7
johnalain <mburn9605@gmail.com>
2025-12-01 11:37:14 +0200

    modules import ways

---
+++ builtmodule.py
@@ -0,0 +1,155 @@
+# print(help("modules"))
+"""AppKit              anaconda_cloud_auth importlib_metadata  re
+Cocoa               annotated_types     inflect             readchar
+CoreFoundation      antigravity         inspect             readline
+Foundation          anyio               io                  referencing
+IPython             appnope             ipaddress           reprlib
+__future__          archspec            ipykernel           requests
+__hello__           argon2              ipykernel_launcher  resource
+__phello__          argparse            ipython_pygments_lexers rfc3339_validator
+_abc                array               ipywidgets          rfc3986_validator
+_aix_support        arrow               isoduration         rfc3987_syntax
+_android_support    ast                 itertools           rich
+_apple_support      asttokens           jedi                rlcompleter
+_argon2_cffi_bindings async_lru           jinja2              rpds
+_ast                asyncio             jovian              runpy
+_asyncio            atexit              json                sched
+_bisect             attr                json5               secrets
+_blake2             attrs               jsonpatch           select
+_bz2                autocommand         jsonpointer         selectors
+_cffi_backend       babel               jsonschema          semver
+_codecs             backports           jsonschema_specifications send2trash
+_codecs_cn          base64              jupyter             setuptools
+_codecs_hk          bdb                 jupyter_client      shellingham
+_codecs_iso2022     binascii            jupyter_console     shelve
+_codecs_jp          bisect              jupyter_core        shlex
+_codecs_kr          bleach              jupyter_events      shutil
+_codecs_tw          boltons             jupyter_lsp         signal
+_collections        brotlicffi          jupyter_server      site
+_collections_abc    bs4                 jupyter_server_terminals sitecustomize
+_colorize           builtins            jupyterlab          six
+_compat_pickle      builtmodule         jupyterlab_pygments smtplib
+_compression        bz2                 jupyterlab_server   sniffio
+_contextvars        cProfile            jupyterlab_widgets  socket
+_csv                cached_property     jwt                 socketserver
+_ctypes             calendar            keyring             socks
+_ctypes_test        certifi             keyword             sockshandler
+_curses             cffi                lark                soupsieve
+_curses_panel       charset_normalizer  libmambapy          sqlite3
+_datetime           click               linecache           sre_compile
+_dbm                cmath               locale              sre_constants
+_decimal            cmd                 logging             sre_parse
+_distutils_hack     code                lzma                ssl
+_elementtree        codecs              mailbox             stack_data
+_functools          codeop              main                stat
+_hashlib            collections         markdown_it         statistics
+_heapq              colorama            markupsafe          string
+_imp                colorsys            marshal             stringprep
+_interpchannels     comm                math                struct
+_interpqueues       compileall          matplotlib_inline   subprocess
+_interpreters       concurrent          mdurl               symtable
+_io                 conda               menuinst            sys
+_ios_support        conda_anaconda_telemetry mimetypes           sysconfig
+_json               conda_anaconda_tos  mistune             syslog
+_locale             conda_content_trust mmap                tabnanny
+_lsprof             conda_env           modialize           tarfile
+_lzma               conda_libmamba_solver modulefinder        tempfile
+_markupbase         conda_package_handling more_itertools      terminado
+_md5                conda_package_streaming multiprocessing     termios
+_multibytecodec     configparser        nbclient            test
+_multiprocessing    contextlib          nbconvert           test_pycosat
+_opcode             contextvars         nbformat            textwrap
+_opcode_metadata    copy                nest_asyncio        this
+_operator           copyreg             netrc               threading
+_osx_support        cryptography        notebook            time
+_pickle             csv                 notebook_shim       timeit
+_posixshmem         ctypes              ntpath              tinycss2
+_posixsubprocess    curses              nturl2path          tkinter
+_py_abc             dataclasses         numbers             token
+_pydatetime         datetime            numpy               tokenize
+_pydecimal          dateutil            objc                tomli
+_pyio               dbm                 opcode              tomllib
+_pylong             debugpy             operator            tornado
+_pyrepl             decimal             optparse            tqdm
+_queue              decorator           os                  trace
+_random             defusedxml          overrides           traceback
+_ruamel_yaml        difflib             packaging           tracemalloc
+_scproxy            dis                 pandas              traitlets
+_sha1               distro              pandocfilters       truststore
+_sha2               doctest             parso               tty
+_sha3               dotenv              pathlib             turtle
+_signal             email               pdb                 turtledemo
+_sitebuiltins       encodings           pexpect             typeguard
+_socket             ensurepip           pickle              typer
+_sqlite3            enum                pickleshare         types
+_sre                errno               pickletools         typing
+_ssl                exceptiongroup      pip                 typing_extensions
+_stat               executing           pkce                typing_inspection
+_statistics         fastjsonschema      pkg_resources       typing_utils
+_string             faulthandler        pkgutil             tzdata
+_strptime           fcntl               platform            unicodedata
+_struct             filecmp             platformdirs        unittest
+_suggestions        fileinput           plistlib            uri_template
+_symtable           fnmatch             pluggy              urllib
+_sysconfig          fqdn                poplib              urllib3
+_sysconfigdata__darwin_darwin fractions           posix               uuid
+_sysconfigdata_x86_64_apple_darwin13_4_0 frozendict          posixpath           venv
+_testbuffer         ftplib              pprint              warnings
+_testcapi           functools           profile             wave
+_testclinic         gc                  prometheus_client   wcwidth
+_testclinic_limited genericpath         prompt_toolkit      weakref
+_testexternalinspection getopt              pstats              webbrowser
+_testimportmultiple getpass             psutil              webcolors
+_testinternalcapi   gettext             pty                 webencodings
+_testlimitedcapi    glob                ptyprocess          websocket
+_testmultiphase     graphlib            pure_eval           wheel
+_testsinglephase    grp                 pwd                 widgetsnbextension
+_thread             gzip                py_compile          wsgiref
+_threading_local    h11                 pyclbr              xml
+_tkinter            h2                  pycosat             xmlrpc
+_tokenize           hashlib             pycparser           xxlimited
+_tracemalloc        heapq               pydantic            xxlimited_35
+_typing             hmac                pydantic_core       xxsubtype
+_uuid               hpack               pydantic_settings   yaml
+_warnings           html                pydoc               zipapp
+_weakref            http                pydoc_data          zipfile
+_weakrefset         httpcore            pyexpat             zipimport
+_xxtestfuzz         httpx               pygments            zipp
+_yaml               hyperframe          pythonjsonlogger    zlib
+_zoneinfo           idlelib             pytz                zmq
+abc                 idna                pyzstd              zoneinfo
+anaconda_anon_usage imaplib             queue               zstandard
+anaconda_auth       import_ipynb        quopri              
+anaconda_cli_base   importlib           random """
+# print(help("math"))
+# import math
+#another way to import module
+# import math as m
+#another way to import module
+# from math import pi
+# import math
+# print(pi)
+# from math import e
+# print(e)
+# a,b,c,d = 1,2,3,4
+# print(e ** a)
+# print(e ** b)
+# print(e ** c)
+# print(e ** d)
+# a,b,c,d,e = 1,2,3,4,5 # type: ignore
+# print(e ** a)
+# print(e ** b)
+# print(e ** c)
+# print(e ** d)
+# print(math.e ** a)
+# print(math.e ** b)
+# print(math.e ** c)
+# print(math.e ** d)
+# print(math.e ** e)
+import example
+result = example.pi
+result1 = example.area(2)
+result2 = example.circumference(3)
+print(result)
+print(result1)
+print(result2)
